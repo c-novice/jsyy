f2974514101113bee7fc0668b104e6316c3e405a
lzq <1312702147@qq.com>
2021-10-02 15:47:57 +0800

    2021.10.02

---
+++ jsyy-admin/src/views/building/buildingInfo/list.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-table :data="tableData" stripe style="width: 100%">
+    <el-table-column prop="date" label="设施id" width="180"></el-table-column>
+    <el-table-column prop="name" label="设施名称" width="180"></el-table-column>
+    <el-table-column prop="address" label="设施预约信息"></el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 设施列表
+      tableData: null
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
